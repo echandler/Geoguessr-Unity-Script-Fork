f77484a37795a9b15c2b7892b3d3fa0cf7a004e2
echandler <chandleric@hotmail.com>
2023-12-23 14:08:00 -0800

    Create unity.meta.js

---
+++ unity.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name          Geoguessr Unity Script
+// @description   For a full list of features included in this script, see this document https://docs.google.com/document/d/18nLXSQQLOzl4WpUgZkM-mxhhQLY6P3FKonQGp-H0fqI/edit?usp=sharing
+// @version       7.0.3
+// @author        Jupaoqq
+// @match         https://www.geoguessr.com/*
+// @run-at        document-start
+// @license       MIT
+// @namespace     https://greasyfork.org/users/838374
+// @grant         none
+// ==/UserScript==
