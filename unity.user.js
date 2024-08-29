dedf74031647747f89d6ee8f172ff79fd879c2a3
echandler <chandleric@hotmail.com>
2024-08-28 22:55:08 -0700

    Made a unity fetch function for the random map challenge external script. v7.4.0.9

--- unity.user.js
+++ unity.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Geoguessr Unity Script
 // @description   For a full list of features included in this script, see this document https://docs.google.com/document/d/18nLXSQQLOzl4WpUgZkM-mxhhQLY6P3FKonQGp-H0fqI/edit?usp=sharing
-// @version       7.4.0.8
+// @version       7.4.0.9
 // @author        Jupaoqq
 // @match         https://www.geoguessr.com/*
 // @run-at        document-start
@@ -14,7 +14,16 @@
 // ==/UserScript==
 
     // Added by EC
-   checkForRanomMapChallenge(); 
+window._unity_fetch_ = window.fetch;
+
+window.fetch = (function(){
+    return async function (...args){
+        return window._unity_fetch_.apply(window, args); 
+    };
+})();
+
+    // Added by EC
+checkForRanomMapChallenge(); 
 
 /**
  * Custom your YouTube Search here!
@@ -301,7 +310,7 @@ var MAPILLARY_API_KEY_LIST =
 var MAPILLARY_API_KEY = MAPILLARY_API_KEY_LIST[Math.floor(Math.random() * MAPILLARY_API_KEY_LIST.length)];
 var MAPY_API_KEY = "placeholder";
 
-console.log("Geoguessr Unity Script v7.4.0.8 by Jupaoqq");
+console.log("Geoguessr Unity Script v7.4.0.9 by Jupaoqq");
 
 
 // Store each player instance
@@ -2119,7 +2128,7 @@ function UnityInitiate() {
     var infoBtn = document.createElement("Button");
     infoBtn.classList.add("unity-btn", "info-btn", "full", "vertical-1", "extra-height", "unity-button-nonclickable");
     infoBtn.id = "Info Button";
-    infoBtn.innerHTML = "Geoguessr Unity Script<font size=1><br>&#169; Jupaoqq | v7.4.0.8</font>";
+    infoBtn.innerHTML = "Geoguessr Unity Script<font size=1><br>&#169; Jupaoqq | v7.4.0.9</font>";
     document.body.appendChild(infoBtn);
     //     infoBtn.addEventListener("click", () => {
     //         window.open('https://docs.google.com/document/d/18nLXSQQLOzl4WpUgZkM-mxhhQLY6P3FKonQGp-H0fqI/edit?usp=sharing');
