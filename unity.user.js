755b4465e6191d4f5f61dbd382943d4edbd46ae1
echandler <chandleric@hotmail.com>
2025-03-21 15:11:14 -0700

    Removed "test" from @name. v7.4.2.6

--- unity.user.js
+++ unity.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name          Geoguessr Unity Script test
+// @name          Geoguessr Unity Script
 // @description   For a full list of features included in this script, see this document https://docs.google.com/document/d/18nLXSQQLOzl4WpUgZkM-mxhhQLY6P3FKonQGp-H0fqI/edit?usp=sharing
-// @version       7.4.2.5
+// @version       7.4.2.6
 // @author        Jupaoqq
 // @match         https://www.geoguessr.com/*
 // @run-at        document-start
