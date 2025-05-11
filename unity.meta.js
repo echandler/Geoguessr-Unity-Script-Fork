dba8bcd5e38085b0178dc45072e9241c9c854e79
echandler <chandleric@hotmail.com>
2025-05-10 17:53:08 -0700

    v7.4.3.0

--- unity.meta.js
+++ unity.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Geoguessr Unity Script
 // @description   For a full list of features included in this script, see this document https://docs.google.com/document/d/18nLXSQQLOzl4WpUgZkM-mxhhQLY6P3FKonQGp-H0fqI/edit?usp=sharing
-// @version       7.4.2.9
+// @version       7.4.3.0
 // @author        Jupaoqq
 // @match         https://www.geoguessr.com/*
 // @run-at        document-start
